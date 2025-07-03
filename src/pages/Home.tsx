bb862caaeb5b31fac3cf60c099512750e0c15933
origin <cadorinisrael@gmail.com>
2025-07-03 01:28:13 -0300

    Home page skeleton

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import '../styles/home.css';
+
+export default function Home() {
+  return (
+    <div className="main-content">
+      <div className="container">
+        <h1>Bem-vindo ao Alongaí!</h1>
+        <p>Vamos começar seu primeiro alongamento.</p>
+      </div>
+    </div>
+  );
+}
